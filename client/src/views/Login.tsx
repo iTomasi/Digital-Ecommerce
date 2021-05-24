28726d3d7c5c41c72604c943daeab6524c162756
iTomasi <duclosiw@gmail.com>
2021-05-24 17:23:37 -0400

    login, register validations && functionalities added (Client)

--- client/src/views/Login.tsx
+++ client/src/views/Login.tsx
@@ -1,10 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
 import config from "../config/config";
 import "./scss/form.scss";
 
 const Login = () => {
+
+    const [showPassword, setShowPassword] = useState<boolean>(false);
+
+    const handlePassword = () => showPassword ? setShowPassword(false) : setShowPassword(true);
+
+    const loggin = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const formData = new FormData(e.currentTarget);
+
+        if (!formData.get("username")) return console.log("Insert your username")
+        else if (!formData.get("password")) return console.log("Insert your password")
+
+        console.log("Waiting for back-end");
+    }
+
     return (
-        <form className="iw_form">
+        <form className="iw_form" onSubmit={loggin}>
             <div className="formSection">
                 <label>Username or Email</label>
                 <input type="text" placeholder="Username or Email" name="username"/>
@@ -14,8 +29,8 @@ const Login = () => {
                 <label>Password</label>
                 
                 <div className="inputPassword">
-                    <input type="password" placeholder="Password..." name="password"/>
-                    <i className={config.EYE.hidde}></i>
+                    <input type={showPassword ? "text" : "password"} placeholder="Password..." name="password"/>
+                    <i className={showPassword ? config.EYE.unhidde : config.EYE.hidde} onClick={handlePassword}></i>
                 </div>
             </div>
 
--- client/src/views/Register.tsx
+++ client/src/views/Register.tsx
@@ -1,11 +1,72 @@
-import React from "react";
+import React, {useState} from "react";
 import config from "../config/config";
 import "./scss/form.scss";
 
+interface IShowPassword {
+    password: boolean,
+    confirm_password: boolean
+}
 
 const Register = () => {
+    const [showPassword, setShowPassword] = useState<IShowPassword>({
+        password: false,
+        confirm_password: false
+    })
+
+    const [fileName, setFileName] = useState<string>("Select an IMG...");
+
+    const handlePassword = (e: any) => {
+        const getInputName = e.currentTarget.previousElementSibling.name;
+
+        setShowPassword((prev: any) => (
+            {
+                ...prev,
+                [getInputName] : prev[getInputName] ? false : true
+            }
+        ))
+    }
+
+    const handleInputFile = (e: any) => {
+        try {
+            const getFileName = e.currentTarget.files[0];
+
+            if (getFileName.type === "image/jpeg" || getFileName.type === "image/png" || getFileName.type === "image/gif" || getFileName.type === "image/svg+xml") {
+                setFileName(getFileName.name);
+                return
+            }
+
+            e.currentTarget.value = null;
+            setFileName("Select an IMG...")
+            console.log("Your profile img should be an img..")
+        }
+
+        catch(e) {
+            setFileName("Select an IMG...")
+        }
+    }
+
+    const sendingDatas = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const emailRegExp = new RegExp(/^[A-Za-z0-9_]+@[A-Za-z]+\.[A-Za-z]{2,3}$/g);
+        const passwordRegExp = new RegExp(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[\W])[A-Za-z0-9\W]{5,}$/g);
+
+        const formData = new FormData(e.currentTarget);
+
+        const username: any = formData.get("username");
+        const email: any = formData.get("email");
+        const password: any = formData.get("password");
+        const confirm_password: any = formData.get("confirm_password");
+
+        if (username.length < 3) return console.log("Your username must contains at less 3 characters")
+        else if (!emailRegExp.test(email)) return console.log("invalid email")
+        else if (!passwordRegExp.test(password)) return console.log("your password must contains at less 5 characters and [upper and lower case, numbers and special characters]")
+        else if (password !== confirm_password) return console.log("password not match")
+
+        console.log("waiting for back-end")
+    }
+
     return (
-        <form className="iw_form">
+        <form className="iw_form" onSubmit={sendingDatas}>
             <div className="formSection">
                 <label>Username</label>
                 <input type="text" placeholder="Username..." name="username"/>
@@ -20,8 +81,8 @@ const Register = () => {
                 <label>Password</label>
 
                 <div className="inputPassword">
-                    <input type="password" placeholder="Password..." name="password"/>
-                    <i className={config.EYE.hidde}></i>
+                    <input type={showPassword.password ? "text" : "password"} placeholder="Password..." name="password"/>
+                    <i className={showPassword.password ? config.EYE.unhidde : config.EYE.hidde} onClick={handlePassword}></i>
                 </div>
             </div>
 
@@ -29,15 +90,21 @@ const Register = () => {
                 <label>Confirm Password</label>
                 
                 <div className="inputPassword">
-                    <input type="password" placeholder="Confirm Password..." name="confirm_password"/>
-                    <i className={config.EYE.hidde}></i>
+                    <input type={showPassword.confirm_password ? "text" : "password"} placeholder="Confirm Password..." name="confirm_password"/>
+                    <i className={showPassword.confirm_password ? config.EYE.unhidde : config.EYE.hidde} onClick={handlePassword}></i>
                 </div>
             </div>
 
             <div className="formSection formFile">
                 <label htmlFor="inputFile">IMG</label>
-                <span>Select an IMG...</span>
-                <input id="inputFile" type="file" name="userImg" style={{display: "none"}} />
+                <span>
+                    {
+                        fileName.length >= 13 && fileName !== "Select an IMG"
+                        ? fileName.substring(0, 13) + "..."
+                        : fileName
+                    }
+                </span>
+                <input id="inputFile" type="file" name="userImg" onChange={handleInputFile} style={{display: "none"}} />
             </div>
 
             <button type="submit">Register</button>
